a01cfa5665513f90cb237415a718265cdaead0db
Abdelghafour Lahrache <61706790+Abdelghafour2001@users.noreply.github.com>
2023-02-23 17:25:48 +0100

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
   DiReact,
@@ -15,7 +14,12 @@ import {
   SiMicrosoftsqlserver,
   SiNeo4J,
   SiCsharp,
+  SiAngularjs,
+  SiOracle,
   SiDotnet,
+  SiSpring,
+  SiArduino,
+
 } from "react-icons/si";
 
 function Techstack() {
@@ -24,6 +28,10 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <SiCsharp />
         <h6>C-sharp</h6>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAngularjs />
+        <h6>Angular</h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
@@ -37,25 +45,14 @@ function Techstack() {
         <DiReact />
         <h6>React.js</h6>
       </Col>
-<Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-        <h6>C++</h6>
-      </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
         <h6>Mongodb</h6>
       </Col>
-<Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-        <h6>Node.js</h6>
-      </Col>
+    
       <Col xs={4} md={2} className="tech-icons">
         <DiJava />
         <h6>Java</h6>
-      </Col>
- <Col xs={4} md={2} className="tech-icons">
-        <SiNeo4J />
-        <h6>Neo4j</h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
@@ -66,12 +63,20 @@ function Techstack() {
         <h6>.Net</h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <SiSpring />
+        <h6>Spring</h6>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
         <DiPython />
         <h6>Python</h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiMicrosoftsqlserver />
         <h6>MS Sql Server</h6>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiOracle />
+        <h6>Oracle</h6>
       </Col>
     </Row>
   );
